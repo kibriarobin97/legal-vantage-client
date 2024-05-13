b163f23a7e0ec740d1d91e4710b52a0796716118
kibriarobin97 <kibriarobin97@gmail.com>
2024-05-13 14:12:37 +0600

    edit

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -10,15 +10,14 @@ const Services = () => {
 
     useEffect(() => {
         const getData = async () => {
-          const { data } = await axios(
-            `${
-              import.meta.env.VITE_API_URL
-            }/all-services?search=${search}`
-          )
-          setServices(data)
+            const { data } = await axios(
+                `${import.meta.env.VITE_API_URL
+                }/all-services?search=${search}`
+            )
+            setServices(data)
         }
         getData()
-      }, [ search ])
+    }, [search])
 
     const handleSearch = e => {
         e.preventDefault()
@@ -28,11 +27,21 @@ const Services = () => {
     }
 
     if (services.length === 0) {
-        return <div className="min-h-[calc(100vh-264px)] flex justify-center items-center">
-            <Helmet>
-                <title>Services | Legal-Vantage</title>
-            </Helmet>
-            <h3 className="text-3xl font-bold text-center text-gray-400 text-opacity-70">No Service Matched</h3>
+        return <div className="min-h-[calc(100vh-264px)]">
+            <form onSubmit={handleSearch}>
+                <fieldset className="w-32 mx-auto my-5 space-y-1">
+                    <div className="flex">
+                        <input type="text" name="search" id="search" placeholder="Search" className="flex flex-1 px-1 text-left border rounded-l-md focus:ring-inset dark:border-gray-300 dark:text-gray-800 dark:bg-gray-100 focus:dark:ring-violet-600" />
+                        <input type="submit" value="Search" className="flex items-center px-4 py-3 sm:text-sm cursor-pointer rounded-r-md bg-orange-400 text-white font-semibold" />
+                    </div>
+                </fieldset>
+            </form>
+            <div className="flex justify-center items-center lg:mt-20">
+                <Helmet>
+                    <title>Services | Legal-Vantage</title>
+                </Helmet>
+                <h3 className="text-3xl font-bold text-center text-gray-400 text-opacity-70">No Service Matched</h3>
+            </div>
         </div>
     }
 
